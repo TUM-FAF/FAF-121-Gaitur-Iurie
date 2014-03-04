cb76de17b9457190832095cec1b046c62ec66659
Iura Gaitur <iura.gaitur@gmail.com>
2014-03-05 01:05:50 +0400

    Laboratory work nr_2

--- wp/lab#2/main.cpp
+++ wp/lab#2/main.cpp
@@ -12,13 +12,18 @@ using namespace std;
 
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK    InfoDlgProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam);
 
 
 HINSTANCE hInst;
+HMENU hMenu;
+HWND hwndList;
+char buffer[25];
+WNDPROC oldList,oldScroll;
 char szClassName[ ] = "My app";
-int items, focused = 0;
+int items, focused = 0,backgroundImg = 0;
 char *tempLink=NULL;
-char **words;
+static char **words;
 
 
 int WINAPI WinMain (HINSTANCE hThisInstance,
@@ -29,7 +34,8 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
     WNDCLASSEX wincl;        /* Data structure for the windowclass */
-
+    hInst = hThisInstance;
+    hMenu = LoadMenu(hInst,MAKEINTRESOURCE(IDB_MENU));
     /* The Window structure */
     wincl.hInstance = hThisInstance;
     wincl.lpszClassName = szClassName;
@@ -45,7 +51,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wincl.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
-    wincl.hbrBackground = CreatePatternBrush(LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BACKGROUND_1)));
+    wincl.hbrBackground = (HBRUSH)TRANSPARENT;
 
     /* Register the window class, and if it fails quit the program */
     if (!RegisterClassEx (&wincl))
@@ -62,7 +68,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
            600,                 /* The programs width */
            400,                 /* and height in pixels */
            HWND_DESKTOP,        /* The window is a child-window to desktop */
-           NULL,                /* No menu */
+           hMenu,                /*Menu */
            hThisInstance,       /* Program Instance handler */
            NULL                 /* No Window Creation data */
            );
@@ -90,7 +96,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    static HWND hwndButtonAdd,hwndButtonDel,hwndTxtList,hwndTxtInput;
+    static HWND hwndButtonAdd,hwndButtonDel,hwndTxtList,hwndTxtInput,hwndScrollH,hwndScrollW,hwndScrollC;
     static HFONT textFont,buttonFont,hFontOld;
     RECT rect;
     HDC hdc;
@@ -100,14 +106,14 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     int width,randWidth,minWidth,maxWidth;
     int textsize;
     int height,randHeight,minHeight,maxHeight;
-    static int cxChar, cyChar;
+    static int cxChar, cyChar,posX,posY;
     static int counter = 210;
     int hours,minutes,seconds;
     static int score;
 
     char szBuffer[200];
     char stBuffer[20];
-    char* wordGuess = NULL;
+    static char* wordGuess = NULL;
     char * placeholder = new char[26];
     char * messag = new char[100];
     static char *cpscore;
@@ -153,10 +159,9 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
             hwndTxtList = CreateWindowEx(
                 (DWORD)NULL,
-                TEXT("edit"), // The class name required is edit
+                TEXT("listbox"), // The class name required is edit
                 TEXT(""), // Default text.
-                WS_VISIBLE | WS_CHILD | WS_BORDER | WS_VSCROLL |
-                ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY, // Textbox styles
+                WS_VISIBLE|WS_CHILD|WS_VSCROLL|LBS_NOINTEGRALHEIGHT|WS_BORDER|LBS_NOTIFY|ES_READONLY,
                 5, 20, // the left and top co-ordinates
                 350, 130, // width and height
                 hwnd, // parent window handle
@@ -175,6 +180,58 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                 (HMENU)IDC_TEXT_INPUT, // the ID of your editbox
                 hInst, // the instance of your application
                 NULL);
+
+            hwndScrollC = CreateWindowEx(
+                (DWORD)NULL,
+                TEXT("scrollbar"),
+                TEXT(""),
+                WS_VISIBLE|WS_CHILD|SBS_HORZ,
+                0,325,
+                150,20,
+                hwnd,
+                (HMENU)ID_SCROLL_HPOS,
+                hInst,
+                NULL);
+
+
+
+
+            hwndScrollH = CreateWindowEx(
+                (DWORD)NULL,
+                TEXT("scrollbar"),
+                TEXT(""),
+                WS_VISIBLE|WS_CHILD|SBS_HORZ,
+                352,325,
+                150,20,
+                hwnd,
+                (HMENU)ID_SCROLL_HPOS,
+                hInst,
+                NULL);
+
+
+            hwndScrollW = CreateWindowEx(
+                (DWORD)NULL,
+                TEXT("scrollbar"),
+                TEXT(""),
+                WS_VISIBLE|WS_CHILD|SBS_VERT,
+                482,175,
+                20,150,
+                hwnd,
+                (HMENU)ID_SCROLL_HPOS,
+                hInst,
+                NULL);
+
+                SetScrollRange(hwndScrollC,SB_CTL,0,255,FALSE);
+                SetScrollPos(hwndScrollC,SB_CTL,0,FALSE);
+
+                SetScrollRange(hwndScrollW,SB_CTL,0,200,FALSE);
+                SetScrollPos(hwndScrollW,SB_CTL,200,FALSE);
+
+                SetScrollRange(hwndScrollH,SB_CTL,0,200,FALSE);
+                SetScrollPos(hwndScrollH,SB_CTL,0,FALSE);
+
+
+                hwndList = hwndTxtList;
                 tempLink = wordGuess;
                 //SetTimer(hwnd,TIMER_GAME,3*60*1000,)
                 SetTimer(hwnd,TIMER_TIME,1000,NULL);
@@ -182,8 +239,80 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         }
         break;
 
+        case WM_VSCROLL:
+        {
+            if((HWND)lParam==hwndScrollW)
+            {
+                switch(LOWORD(wParam))
+                {
+                case SB_PAGEDOWN:
+                    posY-=Y_PAGE_MOVE;
+                case SB_LINEDOWN:
+                    posY=max(0,posY-Y_LINE_MOVE);
+                    break;
+                case SB_PAGEUP:
+                    posY+=Y_PAGE_MOVE;
+                case SB_LINEUP:
+                    posY=min(200,posY+Y_LINE_MOVE);
+                    break;
+                case SB_TOP:
+                    posY=200;
+                    break;
+                case SB_BOTTOM:
+                    posY=0;
+                    break;
+                case SB_THUMBPOSITION:
+                    posY=HIWORD(wParam);
+                    break;
+                default: break;
+                }
+
+                SetScrollPos(hwndScrollW,SB_CTL,200-posY,TRUE);
+                SetWindowPos(hwnd,HWND_TOP,posX,posY,0,0,SWP_SHOWWINDOW|SWP_NOSIZE);
+            }
+        }
+        break;
+
+        case WM_HSCROLL:
+        {
+            switch(LOWORD(wParam))
+                {
+                case SB_PAGELEFT:
+                    posX-=X_PAGE_MOVE;
+                case SB_LINELEFT:
+                    posX=max(0,posX-X_LINE_MOVE);
+                    break;
+                case SB_PAGERIGHT:
+                    posX+=X_PAGE_MOVE;
+                case SB_LINERIGHT:
+                    posX=min(200,posX+X_LINE_MOVE);
+                    break;
+                case SB_RIGHT:
+                    posX=200;
+                    break;
+                case SB_LEFT:
+                    posX=0;
+                    break;
+                case SB_THUMBPOSITION:
+                    posX=HIWORD(wParam);
+                    break;
+                default: break;
+                }
+                SetScrollPos(hwndScrollH,SB_CTL,posX,TRUE);
+                SetWindowPos(hwnd,HWND_TOP,posX,posY,0,0,SWP_SHOWWINDOW|SWP_NOSIZE);
+        }
+        break;
+
         case WM_COMMAND:
         {
+            if(LOWORD(wParam)==IDC_TEXT_LIST && HIWORD(wParam)==LBN_DBLCLK)
+            {
+                int i=SendMessage(hwndList,LB_GETCURSEL,0,0);
+                sprintf(buffer,"Prefix -> %s \nWord -> %s",wordGuess,words[i]);
+                MessageBox(hwnd,buffer,TEXT("Word"),MB_ICONASTERISK|MB_OK);
+                SetFocus(hwndList);
+
+            }
              switch (LOWORD(wParam))
             {
                 case IDC_ADD_BUTTON:
@@ -200,9 +329,9 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                         else
                             strcpy(item, " - "); // Managing the new string
                         strcat(item, messag);
-                        SendMessage(hwndTxtList, EM_REPLACESEL,
-                            TRUE, (LPARAM)item); // Appending a new item in the list
-                        SendMessage(hwndTxtInput, WM_SETTEXT, TRUE,(LPARAM)"");// Clearing the text input
+                        int pos = (int)SendMessage(hwndTxtList, LB_ADDSTRING, 0, (LPARAM)item); // Appending a new item in the list
+                        SendMessage(hwndList, LB_SETITEMDATA, pos, (LPARAM) items);
+                        SendMessage(hwndList, WM_SETTEXT, TRUE,(LPARAM)"");// Clearing the text input
                         delete [] item; // Managing the memory
                         items += 1; // Incrementing the number of items
                         SendMessage(
@@ -258,39 +387,73 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                       }
                 }
                 break;
+                case IDB_INFO:
+                {
+                    DialogBox(hInst,MAKEINTRESOURCE(IDB_DIALOG),hwnd,InfoDlgProc);
+                }
+                break;
+                case IDB_EXIT:
+                {
+                    SendMessage(hwnd,WM_DESTROY,0,0);
+                }
+                break;
+                case IDB_RESTART:
+                {
+
+                }
+                break;
+                case IDB_BACKGROUND_1:
+                {
+                    printf("Frist background");
+                    if(backgroundImg == 0)
+                        break;
+                    backgroundImg = 0;
+                    RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE);
+
+                }
+                break;
+                case IDB_BACKGROUND_2:
+                {
+                    printf("Frist background");
+                    if(backgroundImg == 1)
+                        break;
+                    backgroundImg = 1;
+                    RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE);
+
+                }
+                break;
+                case IDB_RECORDS:
+                {
+                    DialogBox(hInst,MAKEINTRESOURCE(IDB_DIALOG),hwnd,InfoDlgProc);
+                }
+                break;
             }
         }
         break;
 
-        case IDC_ADD_BUTTON:
-        {
 
-
-        }
-        break;
-
-        case WM_CLOSE:
-        {
-                RECT rect;
-                GetWindowRect(hwnd,&rect);
-                width = rect.right - rect.left;
-                height = rect.bottom - rect.top;
-
-                maxWidth = GetSystemMetrics(SM_CXSCREEN) - width;
-                maxHeight = GetSystemMetrics(SM_CYSCREEN) - height;
-                randWidth = rand()%(maxWidth);
-                randHeight = rand()%(maxHeight);
-
-                MoveWindow(hwnd,randWidth,randHeight,width,height,TRUE);
-            }
-            break;
-
-            case WM_DESTROY:
+            case WM_KEYDOWN:
             {
-                KillTimer(hwnd, TIMER_GAME);
-                MessageBox(NULL,"Whyyyyyyyy :D ","Message",MB_OK);
-                PostQuitMessage (0);
-            }    /* send a WM_QUIT to the message queue */
+                switch(wParam)
+                {
+                    case VK_SPACE:
+                        if(GetAsyncKeyState(VK_LCONTROL))
+                        {
+                            if(backgroundImg == 0)backgroundImg = 1;
+                            else if(backgroundImg == 1) backgroundImg = 0;
+                            RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE);
+                        }
+                    break;
+
+                    case KEY_Q:
+                        if(GetAsyncKeyState(VK_LCONTROL))
+                        {
+                            SendMessage(hwnd,WM_DESTROY,0,0);
+                        }
+                    break;
+
+                }
+            }
             break;
 
             case WM_GETMINMAXINFO:
@@ -331,19 +494,21 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             }
             break;
 
-            case WM_SETFOCUS:
-            {
-                //SetFocus(hwndTxtList[0]);
-            }
-            break;
-
             case WM_PAINT:
             {
                 PAINTSTRUCT ps;
                 hdc = BeginPaint(hwnd,&ps);
                 GetClientRect(hwnd,&rect);
-
-                HBITMAP backgrBMP = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BACKGROUND_1));
+                HBITMAP backgrBMP = NULL;
+                if(backgroundImg == 0)
+                {
+                    backgrBMP = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BACKGROUND_1));
+                }
+                else if(backgroundImg == 1)
+                {
+                    backgrBMP = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BACKGROUND_2));
+                }
+                if(!backgrBMP){MessageBox(NULL,"Whyyyyyyyy Backgound? :D ","Message",MB_OK);}
                 HBRUSH backgrBrush = CreatePatternBrush(backgrBMP);
 
                 HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, backgrBrush);
@@ -371,8 +536,8 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                     EnableWindow( GetDlgItem(hwnd, 101),  true);
                     KillTimer(hwnd,TIMER_TIME);
                 }
-                SetBkColor(hdc,TRANSPARENT);
                 SetTextColor(hdc, RGB(152, 0, 16));
+                SetBkColor(hdc,TRANSPARENT);
                 DrawText(hdc,"Done with pride by Iura Gaitur",-1,&rect,DT_CENTER|DT_BOTTOM|DT_SINGLELINE);
                 TextOut(hdc,5,165,"Words that starts with",strlen("Words that starts with"));
                 SetBkColor(hdc,RGB(255,255,255));
@@ -396,12 +561,74 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                 }
             }
             break;
+
+            case WM_CLOSE:
+        {
+                RECT rect;
+                GetWindowRect(hwnd,&rect);
+                width = rect.right - rect.left;
+                height = rect.bottom - rect.top;
+
+                maxWidth = GetSystemMetrics(SM_CXSCREEN) - width;
+                maxHeight = GetSystemMetrics(SM_CYSCREEN) - height;
+                randWidth = rand()%(maxWidth);
+                randHeight = rand()%(maxHeight);
+
+                MoveWindow(hwnd,randWidth,randHeight,width,height,TRUE);
+            }
+            break;
+
+            case WM_DESTROY:
+            {
+                KillTimer(hwnd, TIMER_GAME);
+                MessageBox(NULL,"Whyyyyyyyy :D ","Message",MB_OK);
+                PostQuitMessage (0);
+            }    /* send a WM_QUIT to the message queue */
+            break;
+
             default:                      /* for messages that we don't deal with */
                 return DefWindowProc (hwnd, message, wParam, lParam);
     }
 
     return 0;
 }
+
+BOOL CALLBACK    InfoDlgProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
+{
+    switch(message)
+    {
+    case WM_INITDIALOG:
+        return TRUE;
+    case WM_COMMAND:
+        switch(LOWORD(wParam))
+        {
+        case IDOK:
+            EndDialog(hwnd,0);
+            return TRUE;
+
+        }
+    break;
+    }
+    return FALSE;
+}
+
+LRESULT CALLBACK ListProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    /*if(message == WM_KEYDOWN && wParam == KEY_R)
+    {
+         SetFocus(hwndList);
+    }
+    if(message==WM_KEYDOWN && wParam==KEY_R)
+    {
+        int iIndex=SendMessage(hwndList,LB_GETCURSEL,0,0);
+        sprintf(buffer,"Prefix -> %s \nWord -> %s","asdl",words[iIndex]);
+        MessageBox(hwnd,buffer,TEXT("Word"),MB_ICONASTERISK|MB_OK);
+        SetFocus(hwndList);
+    }*/
+    return CallWindowProc(oldList,hwnd,message,wParam,lParam);
+}
+
 
 
 void CreateChilds()
